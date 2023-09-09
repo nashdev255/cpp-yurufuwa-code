090a7ab8796643fdd4e786f12e23b0fd21a0663a
nashdev255 <yutori1107@gmail.com>
2023-09-09 23:50:50 +0900

    neru

---
+++ armyOfNullChar.cpp
@@ -0,0 +1,14 @@
+/**
+ * @name null文字軍団
+ * @author nashdev255
+ * @brief null文字軍団
+ * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char* str = "";
+    return 0;
+}
---
+++ staticTest.cpp
@@ -0,0 +1,13 @@
+/**
+ * @name staticTest
+ * @author nashdev255
+ * @brief staticの有効な使い方
+ * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
