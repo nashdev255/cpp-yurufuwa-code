c9a9127d37aa50524250e21a0a217ee7236c4b4f
nashdev255 <yutori1107@gmail.com>
2023-09-09 23:29:14 +0900

    add javadoc

---
+++ charAscii.cpp
@@ -0,0 +1,15 @@
+/**
+ * @name charで遊ぼう～ASCII編～
+ * @author nashdev255
+ * @brief コード中に'C++'が出てくる美しいコード
+ * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(char C='A';C<'Z';C++) cout << C;
+    for(char c=97;c<123;c++) cout << c;
+    return 0;
+}
